a6e481c3785ac95f07cc1679aebd1b8bf77df42c
Daniel Connelly <dconnelly@butterflynetwork.com>
2021-12-07 00:40:56 -0500

    day7 clean up

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -2,28 +2,25 @@ fn parse(s: &str) -> Vec<i32> {
     s.trim().split(',').map(|s| s.parse().unwrap()).collect()
 }
 
-fn min_dist_sum(xs: &[i32], dist: impl Fn(i32, i32) -> i32) -> i32 {
+fn min_dist_sum(xs: &[i32], f: impl Fn(i32) -> i32) -> i32 {
     let min = *xs.iter().min().unwrap();
     let max = *xs.iter().max().unwrap();
+    let dist = |x: i32, y: i32| f((x - y).abs());
     let dist_sum = |to: i32| xs.iter().map(|&x| dist(x, to)).sum();
     (min..=max).map(dist_sum).min().unwrap()
 }
 
 fn part1(xs: &[i32]) -> i32 {
-    min_dist_sum(xs, |a, b| (a - b).abs())
+    min_dist_sum(xs, |n| n)
 }
 
 fn part2(xs: &[i32]) -> i32 {
-    min_dist_sum(xs, |a, b| {
-        let n = (a - b).abs();
-        (n * (n + 1)) / 2
-    })
+    min_dist_sum(xs, |n| (n * (n + 1)) / 2)
 }
 
 fn main() {
     let path = std::env::args().nth(1).expect("missing input path");
-    let text = std::fs::read_to_string(&path).unwrap();
-    let xs = parse(&text);
+    let xs = parse(&std::fs::read_to_string(&path).unwrap());
     println!("{}", part1(&xs));
     println!("{}", part2(&xs));
 }
