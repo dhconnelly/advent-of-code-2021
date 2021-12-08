24b40fe281e76a8e04a58356506c2887d4d4b284
Daniel Connelly <dconnelly@butterflynetwork.com>
2021-12-08 15:52:00 -0500

    day8

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,9 +1,15 @@
-#[derive(Clone, Copy)]
+use std::collections::HashMap;
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct Digit(u8);
 
 impl Digit {
-    fn len(self) -> u32 {
+    fn len(&self) -> u32 {
         self.0.count_ones()
+    }
+
+    fn contains(&self, other: &Digit) -> bool {
+        self.0 & other.0 == other.0
     }
 }
 
@@ -23,6 +29,60 @@ fn parse(line: &str) -> Entry {
     Entry { input, output }
 }
 
+fn assign(entry: &Entry) -> HashMap<Digit, i32> {
+    let mut map = HashMap::new();
+    let mut rev = HashMap::new();
+    for digit in entry.input.iter() {
+        let val = match digit.len() {
+            2 => Some(1),
+            4 => Some(4),
+            3 => Some(7),
+            7 => Some(8),
+            _ => None,
+        };
+        if let Some(val) = val {
+            map.insert(*digit, val);
+            rev.insert(val, *digit);
+        }
+    }
+
+    for digit in entry.input.iter() {
+        if map.contains_key(digit) {
+            continue;
+        }
+        let val = match digit.len() {
+            6 if digit.contains(rev.get(&4).unwrap()) => 9,
+            6 if digit.contains(rev.get(&1).unwrap()) => 0,
+            6 => 6,
+            5 if digit.contains(rev.get(&1).unwrap()) => 3,
+            _ => continue,
+        };
+        map.insert(*digit, val);
+        rev.insert(val, *digit);
+    }
+
+    for digit in entry.input.iter() {
+        if map.contains_key(digit) {
+            continue;
+        }
+        let val = if rev.get(&6).unwrap().contains(digit) { 5 } else { 2 };
+        map.insert(*digit, val);
+    }
+
+    map
+}
+
+fn entry_value(entry: &Entry) -> i32 {
+    let map = assign(&entry);
+    let digit_value = |digit: &Digit| map.get(digit).unwrap().to_string();
+    let output_value: String = entry.output.iter().map(digit_value).collect();
+    output_value.parse().unwrap()
+}
+
+fn part2(entries: &[Entry]) -> i32 {
+    entries.iter().map(entry_value).sum()
+}
+
 fn part1(entries: &[Entry]) -> usize {
     let is1478 = |digit: &&Digit| matches!(digit.len(), 2 | 3 | 4 | 7);
     let count1478 = |entry: &Entry| entry.output.iter().filter(is1478).count();
@@ -34,14 +94,14 @@ fn main() {
     let text = std::fs::read_to_string(&path).unwrap();
     let entries = text.lines().map(parse).collect::<Vec<Entry>>();
     println!("{}", part1(&entries));
+    println!("{}", part2(&entries));
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    static INPUT: &'static str =
-        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    static INPUT: &'static str = r"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
 fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
@@ -57,5 +117,11 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
     fn test_part1() {
         let entries = INPUT.lines().map(parse).collect::<Vec<Entry>>();
         assert_eq!(26, part1(&entries));
+    }
+
+    #[test]
+    fn test_part2() {
+        let entries = INPUT.lines().map(parse).collect::<Vec<Entry>>();
+        assert_eq!(61229, part2(&entries));
     }
 }
