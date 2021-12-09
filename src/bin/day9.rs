591416f6363858686900e1f3a6d1a25f22b2b0f9
Daniel Connelly <dconnelly@butterflynetwork.com>
2021-12-09 02:20:05 -0500

    day9 map

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,67 +1,60 @@
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
-type Grid = Vec<Vec<i32>>;
-type Pt = (usize, usize);
+type Pt = (i32, i32);
+type Grid = HashMap<Pt, i32>;
 
-fn is_low_point(grid: &Grid, row: usize, col: usize) -> bool {
-    let val = grid[row][col];
-    !(row > 0 && grid[row - 1][col] <= val
-        || row < grid.len() - 1 && grid[row + 1][col] <= val
-        || col > 0 && grid[row][col - 1] <= val
-        || col < grid[0].len() - 1 && grid[row][col + 1] <= val)
+fn is_low_point(g: &Grid, &(i, j): &Pt) -> bool {
+    let val = g.get(&(i, j)).unwrap();
+    [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
+        .into_iter()
+        .all(|nbr| !g.contains_key(&nbr) || g.get(&nbr).unwrap() > val)
 }
 
-fn part1(grid: &Grid) -> i32 {
-    let risk = |i, j| if is_low_point(grid, i, j) { grid[i][j] + 1 } else { 0 };
-    let row_risk = |i| (0..grid[0].len()).map(|j| risk(i, j)).sum::<i32>();
-    (0..grid.len()).map(row_risk).sum()
+fn low_points(g: &Grid) -> Vec<&Pt> {
+    g.keys().filter(|pt| is_low_point(&g, pt)).collect()
 }
 
-fn explore(grid: &Grid, row: usize, col: usize, v: &mut HashSet<Pt>) {
-    let val = grid[row][col];
-    v.insert((row, col));
-    for (i, j) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
-        if row == 0 && i < 0 || row == grid.len() - 1 && i > 0 {
-            continue;
-        }
-        if col == 0 && j < 0 || col == grid[0].len() - 1 && j > 0 {
-            continue;
-        }
-        let nbr_row = (row as i32 + i) as usize;
-        let nbr_col = (col as i32 + j) as usize;
-        let nbr_val = grid[nbr_row][nbr_col];
-        if !v.contains(&(nbr_row, nbr_col)) && nbr_val != 9 && nbr_val > val {
-            explore(grid, nbr_row as usize, nbr_col as usize, v);
-        }
-    }
+fn part1(g: &Grid, pts: &[&Pt]) -> i32 {
+    pts.iter().map(|pt| g.get(pt).unwrap() + 1).sum()
 }
 
-fn part2(grid: &Grid) -> i32 {
-    let mut sizes = BinaryHeap::new();
-    for row in 0..grid.len() {
-        for col in 0..grid[0].len() {
-            if grid[row][col] != 9 {
-                let mut v = HashSet::new();
-                explore(grid, row, col, &mut v);
-                sizes.push(v.len() as i32);
+fn explore(g: &Grid, pt @ &(i, j): &Pt, v: &mut HashSet<Pt>) -> i32 {
+    let val = *g.get(pt).unwrap();
+    v.insert(*pt);
+    for nbr in &[(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)] {
+        if g.contains_key(nbr) {
+            let nbr_val = *g.get(nbr).unwrap();
+            if !v.contains(nbr) && nbr_val != 9 && nbr_val > val {
+                explore(g, nbr, v);
             }
         }
     }
+    v.len() as i32
+}
+
+fn part2(g: &Grid, low_points: &[&Pt]) -> i32 {
+    let size = |&pt| explore(g, pt, &mut HashSet::new());
+    let mut sizes = low_points.iter().map(size).collect::<BinaryHeap<i32>>();
     sizes.pop().unwrap() * sizes.pop().unwrap() * sizes.pop().unwrap()
 }
 
 fn parse(s: &str) -> Grid {
-    let atoi = |ch: u8| (ch - b'0') as i32;
-    let parse_line = |s: &str| s.bytes().map(atoi).collect();
-    s.lines().map(parse_line).collect()
+    let mut g = Grid::new();
+    for (row, line) in s.lines().enumerate() {
+        for (col, val) in line.bytes().enumerate() {
+            g.insert((row as i32, col as i32), (val - b'0') as i32);
+        }
+    }
+    g
 }
 
 fn main() {
     let path = std::env::args().nth(1).expect("missing input path");
     let text = std::fs::read_to_string(&path).unwrap();
     let grid = parse(&text);
-    println!("{}", part1(&grid));
-    println!("{}", part2(&grid));
+    let low_points = low_points(&grid);
+    println!("{}", part1(&grid, &low_points));
+    println!("{}", part2(&grid, &low_points));
 }
 
 #[cfg(test)]
@@ -77,11 +70,13 @@ mod test {
 
     #[test]
     fn test_part1() {
-        assert_eq!(15, part1(&parse(INPUT)));
+        let g = parse(INPUT);
+        assert_eq!(15, part1(&g, &low_points(&g)));
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(1134, part2(&parse(INPUT)));
+        let g = parse(INPUT);
+        assert_eq!(1134, part2(&g, &low_points(&g)));
     }
 }
