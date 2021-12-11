d66e9e577b50411f9253edf17fb9b899280b7ffa
Daniel Connelly <dconnelly@butterflynetwork.com>
2021-12-11 12:32:57 -0500

    day11 part2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,55 +1,43 @@
-use std::collections::{HashMap, VecDeque};
-
 type Pt = (i32, i32);
-type Grid = HashMap<Pt, u8>;
+type Grid = std::collections::HashMap<Pt, u8>;
 
-static DIRS: [Pt; 8] = [
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-    (0, -1),
-    (0, 1),
-    (1, -1),
-    (1, 0),
-    (1, 1),
-];
-
-fn nbrs((row, col): Pt) -> impl Iterator<Item = Pt> {
-    DIRS.iter().map(move |(drow, dcol)| (row + drow, col + dcol))
+fn nbrs(pt @ (row, col): Pt) -> impl Iterator<Item = Pt> {
+    let row_nbrs = move |dr| (-1..=1).map(move |dc| (row + dr, col + dc));
+    (-1..=1).flat_map(row_nbrs).filter(move |nbr| nbr != &pt)
 }
 
 fn step(grid: &mut Grid) -> usize {
-    let mut q = VecDeque::from_iter(grid.keys().copied());
-    while let Some(pt) = q.pop_front() {
+    let mut steps = 0;
+    let mut q = Vec::from_iter(grid.keys().copied());
+    while let Some(pt) = q.pop() {
         if grid.contains_key(&pt) {
             let val = grid.get_mut(&pt).unwrap();
             *val += 1;
             if *val == 10 {
                 q.extend(nbrs(pt));
+                steps += 1;
             }
         }
     }
-    grid.values_mut().fold(0, |mut acc, val| {
-        if *val > 9 {
-            *val = 0;
-            acc += 1
-        }
-        acc
-    })
+    grid.values_mut().filter(|&&mut val| val > 9).for_each(|val| *val = 0);
+    steps
 }
 
-fn part1(mut g: Grid) -> usize {
-    (0..100).fold(0, |acc, _| acc + step(&mut g))
+fn part1(mut grid: Grid) -> usize {
+    (0..100).fold(0, |acc, _| acc + step(&mut grid))
+}
+
+fn part2(mut grid: Grid) -> usize {
+    (1..).find(|_| step(&mut grid) == grid.len()).unwrap()
 }
 
 fn parse(s: &str) -> Grid {
-    let mut g = Grid::new();
-    for (row, line) in s.lines().enumerate() {
-        for (col, ch) in line.bytes().enumerate() {
-            g.insert((row as i32, col as i32), ch - b'0');
-        }
-    }
-    g
+    s.lines().enumerate().fold(Grid::new(), |grid, (row, line)| {
+        line.bytes().enumerate().fold(grid, |mut grid, (col, ch)| {
+            grid.insert((row as i32, col as i32), ch - b'0');
+            grid
+        })
+    })
 }
 
 fn main() {
@@ -57,4 +45,5 @@ fn main() {
     let text = std::fs::read_to_string(&path).unwrap();
     let grid = parse(&text);
     println!("{}", part1(grid.clone()));
+    println!("{}", part2(grid.clone()));
 }
