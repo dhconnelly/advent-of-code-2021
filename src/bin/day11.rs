9740af56525e8f5964a4189f0f3eda90ace84a49
Daniel Connelly <dconnelly@butterflynetwork.com>
2021-12-16 23:38:32 -0500

    add day11 tests

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -46,3 +46,29 @@ fn main() {
     println!("{}", part1(grid.clone()));
     println!("{}", part2(grid.clone()));
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    static INPUT: &'static str = "5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(1656, part1(parse(&INPUT)));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(195, part2(parse(&INPUT)));
+    }
+}
