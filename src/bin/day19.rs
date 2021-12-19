7ee01cbf9246ee13164c99cec4df8475488b4a6b
Daniel Connelly <dconnelly@butterflynetwork.com>
2021-12-19 08:57:04 -0500

    day19 all alignments

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -12,7 +12,7 @@ fn parse(s: &str) -> Vec<Scan> {
     s.split("\n\n").map(|s| s.lines().skip(1).map(parse_pt).collect()).collect()
 }
 
-fn find(scan1: &Scan, scan2: &Scan) -> Option<Vec<Pt3>> {
+fn maybe_align(scan1: &Scan, scan2: &Scan) -> Option<Scan> {
     for (x1, y1, z1) in scan1 {
         for (x2, y2, z2) in scan2 {
             let (dx, dy, dz) = (x1 - x2, y1 - y2, z1 - z2);
@@ -22,9 +22,24 @@ fn find(scan1: &Scan, scan2: &Scan) -> Option<Vec<Pt3>> {
                 .collect();
             let common: Vec<_> =
                 scan1.intersection(&scan2_shifted).copied().collect();
-            if common.len() >= 12 {
-                return Some(common);
+            if common.len() < 12 {
+                continue;
             }
+            for pt @ &(x, y, z) in &scan2_shifted {
+                if !scan1.contains(&pt) && x <= 1000 && y <= 1000 && z <= 1000 {
+                    continue;
+                }
+            }
+            for pt @ &(x, y, z) in scan1 {
+                if !scan2_shifted.contains(&pt)
+                    && x <= 1000
+                    && y <= 1000
+                    && z <= 1000
+                {
+                    continue;
+                }
+            }
+            return Some(scan2_shifted.iter().copied().collect());
         }
     }
     None
@@ -60,22 +75,33 @@ fn rotations(scan: &Scan) -> Vec<Scan> {
     rotations
 }
 
-fn find_with_rotations(scan1: &Scan, scan2: &Scan) -> Option<Vec<Pt3>> {
-    for scan2 in rotations(scan2) {
-        if let Some(beacons) = find(&scan1, &scan2) {
-            return Some(beacons);
+fn align(scans: &mut [Scan]) {
+    let mut aligned = vec![false; scans.len()];
+    aligned[0] = true;
+    for i in 0..scans.len() {
+        for j in 0..scans.len() {
+            if i == j || aligned[j] {
+                continue;
+            }
+            for scan2 in rotations(&scans[j]) {
+                if let Some(scan2) = maybe_align(&scans[i], &scan2) {
+                    scans[j] = scan2;
+                    aligned[j] = true;
+                    break;
+                }
+            }
         }
     }
-    None
 }
 
 fn main() {
     let path = std::env::args().nth(1).expect("missing input path");
     let text = std::fs::read_to_string(&path).unwrap();
-    let scans = parse(&text);
-    for pt in find_with_rotations(&scans[0], &scans[1]).unwrap() {
-        println!("{:?}", pt);
-    }
+    let mut scans = parse(&text);
+    align(&mut scans);
+    let beacons: Scan =
+        scans.iter().flat_map(|scan| scan.iter().copied()).collect();
+    println!("{}", beacons.len());
 }
 
 #[cfg(test)]
