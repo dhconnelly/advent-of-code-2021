0d47969927631db48a01ee6c47dece64a5bd40b2
Daniel Connelly <dhconnelly@gmail.com>
2024-08-18 15:22:18 +0200

    day24 in c: parse/print instrs

---
+++ src/bin/day24.c
@@ -0,0 +1,143 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    INP,
+    ADD,
+    MUL,
+    DIV,
+    MOD,
+    EQL,
+} opcode;
+
+opcode parse_opcode(char* line) {
+    if (strcmp(line, "inp") == 0) {
+        return INP;
+    } else if (strcmp(line, "add") == 0) {
+        return ADD;
+    } else if (strcmp(line, "mul") == 0) {
+        return MUL;
+    } else if (strcmp(line, "mod") == 0) {
+        return MOD;
+    } else if (strcmp(line, "div") == 0) {
+        return DIV;
+    } else if (strcmp(line, "eql") == 0) {
+        return EQL;
+    } else {
+        assert(0);
+    }
+}
+
+const char* str_of_opcode(opcode op) {
+    switch (op) {
+        case INP:
+            return "inp";
+        case ADD:
+            return "add";
+        case MUL:
+            return "mul";
+        case DIV:
+            return "div";
+        case MOD:
+            return "mod";
+        case EQL:
+            return "eql";
+    }
+}
+
+typedef enum {
+    VAR,
+    NUM,
+} param_type;
+
+typedef struct {
+    param_type type;
+    union {
+        char var;
+        int num;
+    } data;
+} param;
+
+param parse_param(char* line) {
+    param p;
+    if ((line[0] >= '0' && line[0] <= '9') || line[0] == '-') {
+        p.type = NUM;
+        p.data.num = atoi(line);
+        return p;
+    } else {
+        p.type = VAR;
+        p.data.var = *line;
+    }
+    return p;
+}
+
+typedef struct {
+    opcode op;
+    char arg1;
+    param arg2;
+} instr;
+
+instr parse_instr(char* line) {
+    instr i;
+
+    /* parse opcode */
+    char* sep = strstr(line, " ");
+    assert(sep != NULL);
+    *sep = '\0';
+    i.op = parse_opcode(line);
+
+    /* parse arg1 */
+    line = sep + 1;
+    sep = strstr(line, " ");
+    if (sep != NULL) *sep = '\0';
+    i.arg1 = line[0];
+
+    /* parse arg2 if present */
+    if (sep != NULL) {
+        line = sep + 1;
+        i.arg2 = parse_param(line);
+    }
+
+    return i;
+}
+
+void print_instr(instr i) {
+    printf("%s %c", str_of_opcode(i.op), i.arg1);
+    if (i.op != INP) {
+        switch (i.arg2.type) {
+            case VAR:
+                printf(" %c", i.arg2.data.var);
+                break;
+            case NUM:
+                printf(" %d", i.arg2.data.num);
+                break;
+        }
+    }
+    printf("\n");
+}
+
+static const int MAXLINE = 32;
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("usage: day24 <filename>\n");
+        exit(1);
+    }
+
+    FILE* input = fopen(argv[1], "r");
+    if (input == NULL) {
+        perror("can't open input file");
+        exit(1);
+    }
+
+    char line[MAXLINE];
+    while (fgets(line, MAXLINE, input) != NULL) {
+        if (line[0] == '\n' || line[0] == '\0') break;
+        instr i = parse_instr(line);
+        print_instr(i);
+    }
+
+    return 0;
+}
